cd15b3ee48c852127ec515728b78edf0117ecc79
unknown <dinielmi912@gmail.com>
2023-10-18 11:36:27 +0700

    while version

--- Praktikum07/WhileKelipatan09.java
+++ Praktikum07/WhileKelipatan09.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
-public class ForKelipatan09 {
-   
+
+public class WhileKelipatan09 {
+
     public static void main(String[] args) {
+    
     Scanner input = new Scanner(System.in);
 
     int bilangan, total, counter;
@@ -11,11 +13,13 @@ public class ForKelipatan09 {
     System.out.println("Masukkan bilangan 1-9: ");
     bilangan = input.nextInt();
 
-    for (int i = 0; i < 50; ++i) {
-        if (i % bilangan == 0) {
-            total += i;
-            ++counter;
-        }
+    int i = 0;
+
+    while (i < 50){
+        if (i % bilangan == 0)
+        total += i;
+        counter++;
+        i++;
     }
 
     System.out.printf("Pada rentang 1-50 %d dengan total %d", counter, bilangan, total);

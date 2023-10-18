75b6dc46f97e2515398dda494dfa0442846c4e4d
unknown <dinielmi912@gmail.com>
2023-10-18 11:01:45 +0700

    jobsheet 7

---
+++ Praktikum07/ForKelipatan09.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ForKelipatan09 {
+   
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int bilangan, total, counter;
+    total = 0;
+    counter = 0;
+
+    System.out.println("Masukkan bilangan 1-9: ");
+    bilangan = input.nextInt();
+
+    for (int i = 0; i < 50; i++) {
+        if (i % bilangan == 0) {
+            total += i;
+            counter++;
+        }
+    }
+
+    System.out.printf("Pada rentang 1-50 %d dengan total %d", counter, bilangan, total);
+
+    }
+}

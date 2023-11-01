a66387909f63b649426f019b2888a2733ffa3f32
unknown <dinielmi912@gmail.com>
2023-11-01 11:24:07 +0700

    Jobsheet 9 Array Nilai

--- Praktikum 09/ArrayBilangan09.java
+++ Praktikum 09/ArrayBilangan09.java
@@ -7,12 +7,12 @@ public class ArrayBilangan09 {
         bil[0] = 5;
         bil[1] = 13;
         bil[2] = -7;
-        bil[3] = 17;
+        bil[3] = 27;
 
         System.out.println(bil[0]);
         System.out.println(bil[1]);
         System.out.println(bil[2]);
         System.out.println(bil[3]);
-        
+
     }
 }

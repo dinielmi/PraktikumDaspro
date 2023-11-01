b08d04b89413140ed9a94c351ff3823c9ba4606e
unknown <dinielmi912@gmail.com>
2023-11-01 13:00:37 +0700

    Jobsheet 9 Array Nilai edited

--- Praktikum 09/ArrayNilai09.java
+++ Praktikum 09/ArrayNilai09.java
@@ -5,15 +5,21 @@ public class ArrayNilai09 {
     Scanner sc = new Scanner(System.in);
 
     int[] nilaiAkhir =  new int[10];
-
-    for (int i = 0; i < 10; i++) {
+    
+    for (int i = 0; i < nilaiAkhir.length; i++) {
         System.out.println("Masukkan nilai akhir mahasiswa ke-" + (i+1) + ": ");
         nilaiAkhir[i] = sc.nextInt();
     }    
 
     for (int i = 0; i < 10; i++) {
-         System.out.println("Nilai akhir mahasiswa ke-" + (i+1) + "adalah" + nilaiAkhir[i] );
+        if (nilaiAkhir[i] > 70){
+             System.out.println("Mahasiswa ke-" + (i+1) + " lulus ");
+        }
+        if (nilaiAkhir[i] < 70) {
+            System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus ");
+        }
+        }
     }
        
 }
-}
+

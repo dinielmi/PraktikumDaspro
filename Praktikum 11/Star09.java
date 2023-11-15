d91630c73239bd986a19a8f3558816daa3e56737
unknown <dinielmi912@gmail.com>
2023-11-15 10:32:56 +0700

    Star

---
+++ Praktikum 11/Star09.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Star09 {
+    public static void main(String[] args) {
+        Scanner sc = new java.util.Scanner(System.in);
+
+        System.out.print("Masukkan jumlah *: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++){
+            System.out.print("*");
+        }
+    }  
+}

75d223148906bc5c11cd216c76ffa6e93059ca46
unknown <dinielmi912@gmail.com>
2023-11-15 13:39:35 +0700

    tugas individu

--- Praktikum 11/NestedLoop09.java
+++ Praktikum 11/NestedLoop09.java
@@ -5,25 +5,36 @@ public class NestedLoop09 {
         Scanner sc = new Scanner(System.in);
 
         double [][] suhu = new double[2][4];
+       // double [] rataRata = new double[5];
 
         for(int i = 0; i < suhu.length; i++){
             System.out.println("KOTA KE-" + (i + 1));
+         //   double temp = 0;
 
             for ( int j = 0; j < suhu[i].length; j++){
-                System.out.print("  Suhu ke-" + (j + 1) + ": ");
+                System.out.print("Suhu ke-" + (j + 1) + ": ");
                 suhu[i][j] = sc.nextInt();
+               // temp += suhu [i][j];
             }
+            // rataRata[i] = temp / suhu[i].length;
+            // System.out.println();
         }
 
         for (int i = 0; i < suhu.length; i++){
             System.out.print("KOTA KE-" + (i + 1) + ": ");
 
-            for (int j = 0; j < suhu[i].length; j++){
-                System.out.print(suhu[i][j] + " ");
-            }
-
-            System.out.println();
+            // for (int j = 0; j < suhu[i].length; j++){
+            //     System.out.print(suhu[i][j] + " ");
+            // }
+            
+            for (double temp : suhu [i]){
+            System.out.println(temp);
         }
+           System.out.println();
+            // System.out.println("Rata Rata Suhu : " + rataRata[i]);
+        }
+
+       
     }
     
 }

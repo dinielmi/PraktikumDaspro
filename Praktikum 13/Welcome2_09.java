7f03ab030adb1ee290fcb62110c01167f1ec0350
unknown <dinielmi912@gmail.com>
2023-11-29 11:29:30 +0700

    function Welcome Message 2

---
+++ Praktikum 13/Welcome2_09.java
@@ -0,0 +1,11 @@
+public class Welcome2_09 {
+    public static void displayWelcomeMessege(String name) {
+        System.out.println("Selamat datang di Politeknik Negeri Malang, " + name);
+    }
+    public static void main(String[] args) {
+        displayWelcomeMessege("Ani");
+        displayWelcomeMessege("Budi");
+        displayWelcomeMessege("Cica");
+        displayWelcomeMessege("Dodi");
+    }
+}
